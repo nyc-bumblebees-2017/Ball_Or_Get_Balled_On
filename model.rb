acdbf72d3e3dfa0f86eff92d74414a2066cd491a
Kyle McDonald & Matthew Kim & Natalie Ruiz <pair+jkmcdonald.kimm5294.nataliecita@devbootcamp.com>
2017-04-14 11:26:16 -0400

    add method search by zipcode

--- model.rb
+++ model.rb
@@ -16,3 +16,13 @@ def search_by_borough(borough)
   end
   markets_by_borough
 end
+
+def search_by_zipcode(zipcode)
+  markets_by_zipcode = []
+  parsed_markets.each do |market|
+    if market["facilityzipcode"].to_i == zipcode
+      markets_by_zipcode << market
+    end
+  end
+  markets_by_zipcode
+end
