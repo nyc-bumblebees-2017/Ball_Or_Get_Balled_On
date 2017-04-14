4b203af1c45ceea9c862fbd5ab04c480f923e420
Kyle McDonald & Matthew Kim & Natalie Ruiz <pair+jkmcdonald.kimm5294.nataliecita@devbootcamp.com>
2017-04-14 12:23:46 -0400

    call methods from view and controller and add user interface

--- controller.rb
+++ controller.rb
@@ -1,3 +1,20 @@
 require_relative 'model'
 require_relative 'view'
 
+welcome_statement
+
+response = search_type_response
+
+case response
+when "zipcode"
+  zipcode_prompt
+  response = zipcode_response
+  markets_zipcode = search_by_zipcode(response)
+  display_market_by_zipcode(markets_zipcode)
+
+when "borough"
+  borough_prompt
+  response = borough_response
+  markets_borough = search_by_borough(response)
+  display_market_by_borough(markets_borough)
+end
