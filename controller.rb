25d1239f0a8a1cb44499f6329e4e23e0025e8fc7
Kyle McDonald & Matthew Kim & Natalie Ruiz <pair+jkmcdonald.kimm5294.nataliecita@devbootcamp.com>
2017-04-14 15:31:07 -0400

    edit controller to add method that displays all

--- controller.rb
+++ controller.rb
@@ -6,15 +6,19 @@ welcome_statement
 response = search_type_response
 
 case response
+when "all"
+  display($parsed_markets)
+when "display all"
+  display($parsed_markets)
 when "zipcode"
   zipcode_prompt
   response = zipcode_response
-  markets_zipcode = search_by_zipcode(response)
+  markets_zipcode = search_by_zipcode(response, $parsed_markets)
   display_market_by_zipcode(markets_zipcode)
 
 when "borough"
   borough_prompt
   response = borough_response
-  markets_borough = search_by_borough(response)
+  markets_borough = search_by_borough(response, $parsed_markets)
   display_market_by_borough(markets_borough)
 end
