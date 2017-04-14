b5e1459ba7748c171c41f262d25d9dd4ea1efe70
Kyle McDonald & Matthew Kim & Natalie Ruiz <pair+jkmcdonald.kimm5294.nataliecita@devbootcamp.com>
2017-04-14 15:31:39 -0400

    turn parsed_markets into global variable

--- model.rb
+++ model.rb
@@ -5,22 +5,22 @@ require 'open-uri'
 
 file = open('https://data.cityofnewyork.us/resource/cw3p-q2v6.json').read
 
-parsed_markets = JSON.parse(file)
+$parsed_markets = JSON.parse(file)
 
-def search_by_borough(borough)
+def search_by_borough(borough, file)
   markets_by_borough = []
-  parsed_markets.each do |market|
-    if market["facilitycity"].downcase == borough.downcase
+  file.each do |market|
+    if market["facilitycity"] == borough.capitalize
       markets_by_borough << market
     end
   end
   markets_by_borough
 end
 
-def search_by_zipcode(zipcode)
+def search_by_zipcode(zipcode, file)
   markets_by_zipcode = []
-  parsed_markets.each do |market|
-    if market["facilityzipcode"].to_i == zipcode
+  file.each do |market|
+    if market["facilityzipcode"] == zipcode
       markets_by_zipcode << market
     end
   end
